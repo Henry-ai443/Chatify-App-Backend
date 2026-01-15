874a3b368bef0306e954b53fa9b81d22dfe0ae1b
Henry-ai443 <henrymaina2024@outlook.com>
2026-01-15 06:05:49 -0500

    CodeBase SetUp

---
+++ src/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config()
+
+//IMPORTS
+import authroutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+
+const app = express()
+
+const PORT = process.env.PORT;
+
+//ENDPOINTS
+app.use("/api/auth", authroutes);
+app.use("/api/messages", messageRoutes)
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})

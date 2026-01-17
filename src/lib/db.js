02ee3359b7eacabd6cc660001c046bdfa2cbe83f
Henry Maina <henrymaina2024@outlook.com>
2026-01-17 13:25:23 +0300

    cr changes

--- src/lib/db.js
+++ src/lib/db.js
@@ -2,8 +2,11 @@ import mongoose from "mongoose";
 
 export const connectDb = async () => {
     try{
-        await mongoose.connect(process.env.MONGO_URI)
-        console.log("MONGODB connected successfully")
+        const {MONGO_URI} = process.env.MONGO_URI;
+        if(!MONGO_URI) throw new Error("MONGO_URI is not set");
+
+        const conn = await mongoose.connect(process.env.MONGO_URI);
+        console.log("MONGODB connected successfully");
     }catch(error){
         console.error("Erro connecting mongo db:", error);
         process.exit(1)
